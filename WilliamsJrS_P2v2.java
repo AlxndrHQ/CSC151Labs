b548426b7a8da9a81966d2bd46ed6d2512547fbe
AlxndrHQ <AlxndrHQ@gmail.com>
2020-05-06 00:42:31 -0400

    see comments at top of v2 driver

--- WilliamsJrS_P2v2.java
+++ WilliamsJrS_P2v2.java
@@ -1,13 +1,16 @@
 import java.util.*;// imports util class
 
-// use a do-while loop to calc new student and their information 
+/* next iteration: 
+random String generator for name, major, class, 
+& random number generator for grades
+*/
 
 public class WilliamsJrS_P2v2
 {
     public static void main(String[]args)
     {
         Scanner keyboard = new Scanner(System.in);// Allows for user input 
-        Stack<WilliamsJrS_Student> stack = new Stack();// make a new ArrayList (initially empty)
+        Stack<WilliamsJrS_Student> stack = new Stack();// make a new stack (initially empty)
 
         String userYes = "yes";
         do
@@ -21,15 +24,37 @@ public class WilliamsJrS_P2v2
             System.out.println("Enter your classification: ");
             String classification = keyboard.nextLine();
     
-            System.out.println("Enter your GPA: ");
-            double GPA = keyboard.nextDouble();
+            //System.out.println("Enter your GPA: ");
+            //double GPA = keyboard.nextDouble();
+
+            System.out.println("Enter the grades for Student 2 (A -> 4.0, A- -> 3.7): ");
+
+            System.out.println("\n\tEnglish grade: (3 cr.): ");
+            double engGrade = keyboard.nextDouble();// 3 credit hours
+            
+            System.out.println("\n\tCommunications grade: (3 cr.): ");
+            double commGrade = keyboard.nextDouble();// 3 credit hours 
+            
+            System.out.println("\n\tComputer Science grade: (3 cr.): ");
+            double csGrade = keyboard.nextDouble();// 3 credit hours
+            
+            System.out.println("\n\tMath grade: (4 cr.): ");
+            double mathGrade = keyboard.nextDouble();// 4 credit hours 
+            
+            System.out.println("\n\tHealth grade: (2 cr.): ");
+            double healthGrade = keyboard.nextDouble();// 2 credit hours 
+            
+            System.out.println("\n\tUniversity grade: (1 cr.): ");
+            double uniGrade = keyboard.nextDouble();// 1 credit hour
     
             // Create a Student object, called firstStudent_XXX. Pass in appropriate variables
             WilliamsJrS_Student student = new WilliamsJrS_Student(name, major, classification);
-            student.changeGPA(GPA);
+            // student.changeGPA(GPA);
+            student.calculateGPA(engGrade, commGrade, csGrade, mathGrade, healthGrade, uniGrade);
+
     
             // Prints out first Student object
-            System.out.println("Student 1 information: " + student.toString());
+            System.out.println(student.getName() + "'s information: " + student.toString());
 
             System.out.println("Would you like to run the test again?");
             keyboard.nextLine();// clears buffer
@@ -45,7 +70,8 @@ public class WilliamsJrS_P2v2
 
             // method 2:
             Object[] vals = stack.toArray();
-            for (Object obj : vals) {
+            for (Object obj : vals) 
+            {
                 System.out.println(obj);// prints each item from the stack from first entered to last entered
                 stack.pop();// removes each item from stack after it has been printed
             }
