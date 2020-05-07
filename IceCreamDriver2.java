157e76c177b74ae7f6d1fea425006bd496dda7d5
AlxndrHQ <AlxndrHQ@gmail.com>
2020-05-07 15:50:33 -0400

    deleted unused CSC151Labs folder. added do while loop and input validation to IceCreamDriver2.
    Added To Do list to Readme.md

--- IceCreamDriver2.java
+++ IceCreamDriver2.java
@@ -13,7 +13,7 @@ public class IceCreamDriver2 {
          */
 
         Scanner keyboard = new Scanner(System.in);
-        DecimalFormat decimalFormat = new DecimalFormat("#,###.##");// creates new Decimal Format object
+        DecimalFormat decimalFormat = new DecimalFormat("#,###.00");// creates new Decimal Format object
 
         String again = "";// used to determine if user wants to run test again or not
 
@@ -44,105 +44,106 @@ public class IceCreamDriver2 {
                     .println(name + ", What ice cream flavor would you like? (Enter the flavor name or a number 1-6) ");
             flavor = keyboard.nextLine();// stores flavor choice that user would like
             switch (flavor) {
-                case "Vanilla", "1":
+                case "1", "Vanilla", "vanilla":
                     System.out.println("\nYou chose Vanilla");
                     scoopCost = 1.50F;
                     // newOrder.setFlavor(1);// stores the flavor to the order
                     // newOrder.setCost(scoopCost);// stores scoopCost to order
                     break;
-                case "Chocolate", "2":
+                case "Chocolate", "2", "chocolate":
                     System.out.println("\nYou chose Chocolate");
                     scoopCost = 0.95F;
                     // newOrder.setFlavor(2);
                     // newOrder.setCost(scoopCost);
                     break;
-                case "Strawberry", "3":
+                case "Strawberry", "3", "strawberry":
                     System.out.println("\nYou chose Strawberry");
                     scoopCost = 2.59F;
                     // newOrder.setFlavor(3);
                     // newOrder.setCost(scoopCost);
                     break;
-                case "Blackberry", "4":
+                case "Blackberry", "4", "blackberry":
                     System.out.println("\nYou chose Blackberry");
                     scoopCost = 1.50F;
                     // newOrder.setFlavor(4);
                     // newOrder.setCost(scoopCost);
                     break;
-                case "Oreo", "5":
+                case "Oreo", "5", "oreo":
                     System.out.println("\nYou chose Oreo");
                     scoopCost = 1.75F;
                     // newOrder.setFlavor(5);
                     // newOrder.setCost(scoopCost);
                     break;
-                case "Rocky Road", "6":
+                case "Rocky Road", "6", "rocky road":
                     System.out.println("\nYou chose Rocky Road");
                     scoopCost = 1.99F;
                     // newOrder.setFlavor(6);
                     // newOrder.setCost(scoopCost);
                     break;
+                default:
+                    System.out.println("\nEnter a valid choice (1-6)");
+                    flavor = keyboard.nextLine();// stores flavor choice that user would like
+                    break;
             }
 
             do {
-                System.out.println("How many scoops would you like? (must be more than 1)");
+                System.out.println("How many scoops would you like? (must be at least 1)");
                 noScoops = keyboard.nextInt();// stores number of scoops
             } while (noScoops < 1);
 
             // prints what the total cost is based on flavor and number of scoops
             totalCost = scoopCost * noScoops;// total cost of ice-cream based on number of scoops and type of ice cream
-            System.out.println("Your order of  " + flavor + " for " + scoopCost + " per scoop costs $"
+            System.out.println("Your order of " + flavor + " for $" + scoopCost + " per scoop costs $"
                     + decimalFormat.format(totalCost) + ".");
 
             // asks user how large of a bill they would like to enter to pay for their bill
             System.out.println("Please enter your bill ($1, $5, $10, $20)");
             bill = keyboard.nextInt();
 
-            while (!(bill == 1 || bill == 5 || bill == 10 || bill == 20)) {// used as long as the bill is not a 1, 5, 10,
-                                                                           // or
-                                                                           // 20
+             do {// used as long as the bill is not a 0, 1, 5, 10, or 20
 
-                System.out.println("Please enter your bill ($1, $5, $10, $20)");
+                System.out.println("Please enter only a $1, $5, $10, or $20");
                 bill = keyboard.nextInt();
-            }
-            
-            double userTender = 0.0;
-            while ((bill == 1 || bill == 5 || bill == 10 || bill == 20)) {
-                userTender = (double) bill;
-                System.out.println("You have " + userTender + " on your books.");
+            } while (!(bill == 0 || bill == 1 || bill == 5 || bill == 10 || bill == 20));
 
-                while ((userTender - totalCost) < 0.0) {
-                    double remaining = totalCost - (double) bill;
-                    System.out.println("Please enter " + remaining + " to complete your order");
-                    userTender += keyboard.nextInt();
+            double userTender = (double) bill;
+            System.out.println("You have entered " + userTender + " dollars. ");
+            double remaining = totalCost - (double) userTender;
+            // System.out.println("You owe an additional: " + remaining);
 
-                    do
-                    {
-                        System.out.println("Please enter your bill ($1, $5, $10, $20)");
-                        userTender += keyboard.nextInt();
-                    } while (!(bill == 1 || bill == 5 || bill == 10 || bill == 20)); // used as long as the bill is not a
-                                                                                   // 1, 5, 10,
-                                                                                   // or
-                                                                                   // 20
+            while ((bill == 0 || bill == 1 || bill == 5 || bill == 10 || bill == 20)) {
+
+                 do {
+                    
+                    System.out.println("Please enter an additional $" + decimalFormat.format(remaining) + " to complete your order");
+                    bill = keyboard.nextInt();
+                    userTender += bill;// adds the bill entered to the user's tender
+                    System.out.println("You have entered $" + userTender + " in total.");
+                    remaining = totalCost - (double) userTender;// subtracts the remaining amount from what the user
+                                                                // entered
+                    // adds a sum of what the user has enetered, takes away from the remaining
+                    // balance
+                    System.out.println("You owe an additional $" + decimalFormat.format(remaining));
+
+                } while (remaining > 0.0);
+
+                if (userTender > remaining) {// userTender was larger than cost 
+                    change = (float) userTender - totalCost;// a floating-point representing a change after paying for
+                                                            // this ice-cream cone
+                    break;
                 }
-                
+
             }
-            
-            change = (float)userTender - totalCost;// = a floating-point representing a change after paying for this ice-cream cone
+
+            change = (float) userTender - totalCost;// = a floating-point representing a change after paying for this
+                                                    // ice-cream cone
             System.out.println("Your change is $" + decimalFormat.format(change));
 
             // needs to calculate how many more scoops you can get
             // remaining scoops = change / price per scoop
             float remainingScoops = change / scoopCost;
-            System.out.println("With your change, you can get " + decimalFormat.format(remainingScoops)
-                    + " more scoops of " + flavor);
-
-            /*
-             * To Do: - needs to create object for this order - Needs to add order object to
-             * stack - needs to print order object from stack
-             */
-            // creates object for this order
-            // IceCream orderName = new IceCream(name, flavor, noScoops, scoopCost, bill,
-            // change);
-            // stack.add(orderName);
+            System.out.println(
+                    "With your change, you can get " + (int) remainingScoops + " more whole scoops of " + flavor);
 
             System.out.println("Would you like to Place another order? (Only enter \"yes\" or \"no\")");
             keyboard.nextLine();// clears buffer
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This will be a collection of all of the labs, projects, and classes that I was g
 
 In this repo I will be recompleting all of my old assignments.
 
-## IceCreamDriver
+## IceCreamDriver2
 
 This is the continuation of the ice cream stand.
 I will use the IceCream class to create user defined methods to set and retrieve the values pertaining to the ice cream stand driver.
@@ -16,6 +16,19 @@ I will use the IceCream class to create user defined methods to set and retrieve
 - store each input based on what the user has entered (Ex. setFlavor)
 - add each order to a stack.
 - at the end, ask if the user would like to place another order
+
+Goal:
+
+- user will input a bill, if insuffient they will be prompted to add the remaining balance (only 1, 5, 10, 20)
+    the additional bills will be added to their tender
+    subtract their tender from their bill, then return amount of change
+
+To Do:
+
+  -  needs to create object for this order
+  - needs to print order object from stack
+  - (Completed) the flavor needs to work for the string or the number value
+    
 
 ## WilliamsJrS_P3
 
