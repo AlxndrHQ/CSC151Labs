677d1a6aa11f20f59849005492e2399da0589469
AlxndrHQ <AlxndrHQ@gmail.com>
2020-05-27 14:34:56 -0400

    configuring program to run c#

---
+++ CSProjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
